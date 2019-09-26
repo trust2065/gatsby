c4fcb880a44aff8a388c20b5e1df8529f71e92c4
Choco <trust2065@gmail.com>
2019-09-27 01:01:10 +1000

    create links dynamically

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,51 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import "../index.scss"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Let's find the most valuable stuff this week!</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/page-3/">Go to page 3</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const {
+    allMarkdownRemark: { edges: postList },
+  } = data
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Let's find the most valuable stuff this week!</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/page-3/">Go to page 3</Link>
+      {postList.map(post => {
+        const { path, title } = post.node.frontmatter
+        const { id } = post.node
+        return (
+          <Link key={id} to={path}>
+            {title}
+          </Link>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
